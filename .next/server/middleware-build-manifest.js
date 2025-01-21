839e85319416092a8e8bacbebabe9f4e57afb605
Justin Cefai <skatemich@yahoo.com>
2025-01-21 10:58:32 -0800

    static header and footer added

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,35 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:[],rootMainFilesTree:{},pages:{"/":["static/chunks/webpack-b66ed8ba6f029a99.js","static/chunks/framework-b96261d959dd50e7.js","static/chunks/main-824eed059b945654.js","static/chunks/pages/index-ab2dc5794e444aa9.js"],"/_app":["static/chunks/webpack-b66ed8ba6f029a99.js","static/chunks/framework-b96261d959dd50e7.js","static/chunks/main-824eed059b945654.js","static/chunks/pages/_app-62ff9594266cbbd0.js"],"/_error":["static/chunks/webpack-b66ed8ba6f029a99.js","static/chunks/framework-b96261d959dd50e7.js","static/chunks/main-824eed059b945654.js","static/chunks/pages/_error-fde50cb7f1ab27e0.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [
+    "static/chunks/react-refresh.js"
+  ],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/index.js"
+    ],
+    "/_app": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_app.js"
+    ],
+    "/_error": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_error.js"
+    ]
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
 const HomePage = () => {
   return (
     <div>
+      <Header />
       <h1>Welcome to My Blog</h1>
+      <Footer />
     </div>
   );
 };
