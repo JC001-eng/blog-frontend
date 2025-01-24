75007b177908bf04ffb6b30181ac189bc04f0341
Justin Cefai <skatemich@yahoo.com>
2025-01-24 14:32:08 -0800

    update loading screen component and add header and footer imports

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LoadingScreenContainer } from "./LoadingScreen.styles";
+
+const LoadingScreen: React.FC = () => {
+  return (
+    <LoadingScreenContainer>
+      <p>Loading...</p>
+    </LoadingScreenContainer>
+  );
+};
+
+export default LoadingScreen;
